7e6efb544fdfcaa47c7712e0ac24f0d4d0feb886
Link Dupont <link@sub-pop.net>
2020-02-28 13:53:07 -0500

    Raise exception if host lookup finds zero or more than 1 host
    Signed-off-by: Link Dupont <link@sub-pop.net>

--- insights/client/connection.py
+++ insights/client/connection.py
@@ -1020,6 +1020,12 @@ class InsightsConnection(object):
         if content is None:
             return None
 
+        host_details = json.loads(content)
+        if host_details["total"] < 1:
+            raise Exception("Error: no host detected (insights_id = %s): Run insights-client --status to check registration status" % generate_machine_id())
+        if host_details["total"] > 1:
+            raise Exception("Error: multiple hosts detected (insights_id = %s)" % generate_machine_id())
+
         if not os.path.exists("/var/lib/insights"):
             os.makedirs("/var/lib/insights", mode=0o755)
 
@@ -1027,7 +1033,7 @@ class InsightsConnection(object):
             f.write(content)
             logger.debug("Wrote \"/var/lib/insights/host-details.json\"")
 
-        host_id = json.loads(content)["results"][0]["id"]
+        host_id = host_details["results"][0]["id"]
         url = self.base_url + "/insights/v1/system/%s/reports/" % host_id
         content = self._get(url)
         if content is None:
